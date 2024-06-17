37878d90b218086f89c1a5e9d189e7e53efa9d59
Christopher Damour <129872252+damourChris@users.noreply.github.com>
2024-06-17 14:56:03 +0000

    feat: add run_preprocessing function and export it

--- preprocessing/main.jl
+++ preprocessing/main.jl
@@ -1,5 +1,20 @@
 module Preprocessing
 
+
+using ..Utils: get_preprocessing_env, get_preprocessing_script
+
+export run_preprocessing
+
+
+function run_preprocessing()
+    # Run the R part of preprocessing
+    cmd = get_preprocessing_env() * "bin/Rscript"
+    script_path = get_preprocessing_script()
+
+    run(`$cmd $script_path`)
+end
+
+
 function __init__()
     # Check if the required environment variables are set
     required_envs_vars = ["CONDA_PREFIX", "R_DIR", "DATA_PATH"]
@@ -11,6 +26,5 @@ function __init__()
     end
 
 end
-
 
 end # module
