c98116aa2fb031241567dce9edcd78a753c772a1
Christopher Damour <129872252+damourChris@users.noreply.github.com>
2024-06-17 21:09:00 +0000

    feat: add initialization function for loading environment variables

--- src/MTR.jl
+++ src/MTR.jl
@@ -6,5 +6,14 @@ using .Utils
 include("./preprocessing/main.jl")
 using .Preprocessing
 
+function __init__()
+    using Requires
+    @requires DotEnv = "4dc1fcf4-5e3b-5448-94ab-0c38ec0385c1" begin
+        @info "Loading environment variables from .env file"
+        using DotEnv
+        DotEnv.load!()
+    end
+
+end
 
 end
