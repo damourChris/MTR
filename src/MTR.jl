cef2997948440c9e54f616125a49691a87e331ad
Christopher Damour <129872252+damourChris@users.noreply.github.com>
2024-08-06 14:58:59 +0000

    fix: point the preprocessing module source code to updated place
    remove useless DotEnv loading

--- src/MTR.jl
+++ src/MTR.jl
@@ -3,17 +3,10 @@ module MTR
 include("./utils/utils.jl")
 using .Utils
 
-include("./preprocessing/main.jl")
+include("./preprocessing/preprocessing.jl")
 using .Preprocessing
 
 function __init__()
-    using Requires
-    @requires DotEnv = "4dc1fcf4-5e3b-5448-94ab-0c38ec0385c1" begin
-        @info "Loading environment variables from .env file"
-        using DotEnv
-        DotEnv.load!()
-    end
-
 end
 
 end
