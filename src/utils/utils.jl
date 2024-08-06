258cb05f0e6231fd96713e9d1ead07e92a7b0451
Christopher Damour <129872252+damourChris@users.noreply.github.com>
2024-08-06 14:34:23 +0000

    feat: add custom conversion methods to Utils module

--- src/utils/utils.jl
+++ src/utils/utils.jl
@@ -3,4 +3,18 @@ module Utils
 include(joinpath(@__DIR__, "preprocessing.jl"))
 export preprocess_data
 
+include(joinpath(@__DIR__, "custom_conversion.jl"))
+export
+       ExpressionSet,
+       fData,
+       fData!,
+       pData,
+       pData!,
+       add_pheno_column!,
+       exprs,
+       featureNames,
+       sampleNames,
+       save_to_r_eset,
+       load_eset
+
 end
